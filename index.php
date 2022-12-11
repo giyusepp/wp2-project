257dd9c8938637189c211380f605e74ceef8e650
giyusepp <giyuseppmail@gmail.com>
2022-12-11 19:08:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Halaman Login</title>
+        <meta charset="utf-8"
+        <meta name="viewport" content="width=device=width, initial-scale=1">
+        <link rel="stylesheet" href="aset/bootstrap/css/bootstrap-grid.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="aset/bootstrap/js/bootstrap.bundle.js"></script>
+    </head>
+    <body>
+
+    <div class="container" align="center">
+        <div align="center" style="width:400px;margin-top:5%;">
+    <form method="POST" class="well well-lg" action="" style="-webkit-box-shadow: 0px 0px 30px #788788;">
+<h3></h3></form></div>
+    </div>
+    </body>
+</html>
